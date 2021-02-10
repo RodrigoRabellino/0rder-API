fe204170b0e29188d12980b6a781290c6604786d
RodrigoRabellino <30850683+RodrigoRabellino@users.noreply.github.com>
2021-02-10 18:57:56 -0300

    add proxy

--- routers/products.js
+++ routers/products.js
@@ -2,9 +2,6 @@ const {Router} = require("express")
 const controller = require("../controllers/products")
 const router = Router()
 
-
 router.get('/', controller.list)
-
-
 
 module.exports = router
